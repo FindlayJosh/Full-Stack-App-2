86c0be69e925ef87c33cdfee3e556bb57f88ab7a
FindlayJosh <joshuafindlay@gmail.com>
2022-05-30 20:05:50 -0400

    more updates

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://jerry:jerry@cluster0.sqkte.mongodb.net/storyboard?retryWrites=true&w=majority', 
+    'dbName': 'storyboard'
+};
